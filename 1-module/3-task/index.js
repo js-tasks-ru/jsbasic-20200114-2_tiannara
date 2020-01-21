49f720deba301237b9d3b695f28f53b4c0250593
Лидия Белова <Tiannara@yandex.ru>
2020-01-22 04:07:06 +0700

    m1t3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,5 +3,10 @@
  * @param {string} str
  * @returns {string}
  */
-function ucFirst(str) {
+function ucFirst(str) {function ucFirst(str) {
+    if (!str) return str;
+  
+    return str[0].toUpperCase() + str.slice(1);
+  }
 }
+alert( ucFirst("вася") ); 
